aa8e2181ca5eabbc7d78a7c83c0fde5d5375f64b
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-12 21:35:39 +0600

    nodemon setup : 5. npm install -g nodemon 6. In order to overcome this error do the following steps  	npm install nodemon 	add this comments in scripts section 	"serve": "nodemon index.js" and the complete code looks like 	“scripts”: {           "serve": "nodemon index.js" 	}, Run following command npm run serve
    5. npm install -g nodemon
    6. In order to overcome this error do the following steps
    
    	npm install nodemon
    	add this comments in scripts section
    	"serve": "nodemon index.js" and the complete code looks like
    	“scripts”: {
              "serve": "nodemon index.js"
    	},
    Run following command
    npm run serve

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const app = express();
 // const rootCall = (req,res) => res.send('Thank you very much')
 
 app.get('/', (req,res)=>{
-    res.send('I know how to open Node... YAY');
+    const fruit = {
+        product: 'ada',
+        price:200
+    }
+    res.send(fruit);
+});
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantity:1000,price:10000});
 })
 
 app.listen(3000, ()=> console.log('Listening to port 3000'))
