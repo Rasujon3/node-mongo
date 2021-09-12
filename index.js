d760efd14f3b96dfd151d242922fda542976b7b5
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-12 21:56:42 +0600

    server e data pathabo & dynamic krbo
    video : 4

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ const app = express();
 // }
 
 // const rootCall = (req,res) => res.send('Thank you very much')
+const users = ["Sujon","Liya","Marufa","Sumon","Arin","Naim"];
+
 
 app.get('/', (req,res)=>{
-    const fruit = {
-        product: 'ada',
-        price:200
-    }
-    res.send(fruit);
+    // const fruit = {
+    //     product: 'ada',
+    //     price:200
+    // },
+    const users = ["Sujon","Liya","Marufa","Sumon","Arin","Naim"];
+
+    // res.send(fruit);
 });
 app.get('/fruits/banana',(req,res)=>{
     res.send({fruit:'banana',quantity:1000,price:10000});
+})
+
+app.get('/users/:id',(req,res)=>{
+    // console.log(req.params);
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
+
 })
 
 app.listen(3000, ()=> console.log('Listening to port 3000'))
